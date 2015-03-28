19c81bf6a4e6e61e804d28d9898701874207bd91
Bill Criswell <bill.criswell@gmail.com>
2015-03-28 12:16:43 -0400

    Updated stats a bit

--- src/js/components/card/index.js
+++ src/js/components/card/index.js
@@ -1,13 +1,19 @@
 var _ = require('lodash');
+var randomSum = require('./random-sum.js');
 
 module.exports = {
+
   replace: true,
+
   template: require('./template.html'),
+
   data: function () {
     return {
-      G: Math.floor(Math.random() * 250)
+      G: Math.floor(Math.random() * 250),
+      TOTAL: Math.floo
     }
   },
+
   methods: {
     random: function (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -18,36 +24,54 @@ module.exports = {
       var nicknames = this.$root.nicknames;
       return _.sample(nicknames, 2).join(' ');
     },
+
     team: function () {
       var teams = this.$root.teams;
       return _.sample(teams);
     },
+
     position: function () {
       var positions = this.$root.positions;
       return _.sample(positions);
     },
+
     bats: function () {
       var bats = this.$root.bats;
       return _.sample(bats);
     },
+
+    number: function () {
+      return this.random(1, 99);
+    },
+
     AB: function () {
       return this.random(this.G, this.G * 1.5);
     },
+
+    HITS: function () {
+      return randomSum(4, this.random(10, this.AB));
+    },
+
     H: function () {
-      return this.random(1, this.AB * (this.random(40, 90) / 100));
+      return this.HITS[0];
     },
+
     B2: function () {
-      return +this.random(1, this.AB);
+      return this.HITS[1];
     },
+
     B3: function () {
-      return this.random(1, this.AB);
+      return this.HITS[2];
     },
+
     HR: function () {
-      return this.random(0, 1000);
+      return this.HITS[3];
     },
+
     RBI: function () { 
       return this.random(0, 10000);
     },
+
     AVG: function () {
       return this.H / this.AB;
     }
